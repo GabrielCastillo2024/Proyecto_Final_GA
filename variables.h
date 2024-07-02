e3982dd4c99dbe1df200ef535149fb17fae4c5cc
denisalvarez <dalvarezp@uamv.edu.ni>
2024-07-02 00:13:57 -0600

    funcion agg clientela

---
+++ variables.h
@@ -0,0 +1,13 @@
+#define VARIABLES_H
+
+const int MAX_REG = 100;
+
+struct CLIENTE {
+    int id;
+    char nombre[50];
+    char correo[50];
+    char telefono[15];
+    char direccion[100];
+    char fecha[20];
+};
+
