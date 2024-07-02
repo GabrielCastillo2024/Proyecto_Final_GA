8b9d59854ea959c72938d063f25ca63625c835bf
acr301 <acrothschuh@uamv.edu.ni>
2024-07-02 14:11:47 -0600

    la tuani tuani con las funcionalidades dale tony

---
+++ intentodos/variables.h
@@ -0,0 +1,18 @@
+#ifndef VARIABLES_H
+#define VARIABLES_H
+
+#include <iostream>
+
+#define MAX_REG 1000
+
+typedef struct CUSTOMER{
+	int id;
+	char email[50];
+	char name[30];
+	char telephone[15];
+	char address[100];
+	char date[20];
+};
+
+#endif
+
